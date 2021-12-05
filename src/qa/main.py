c306920dfd84cfd3ad365b1f3a170cc5d117e3dd
Anh Nguyen <anguye65@uncc.edu>
2021-12-04 22:56:49 -0500

    Allow users to upvote and downvote on posts

--- src/qa/main.py
+++ src/qa/main.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user
 
 from . import db
-from .models import Post, Group, Image
+from .models import Post, Group, Image, Vote
 
 main = Blueprint('main', __name__)
 
@@ -59,7 +59,7 @@ def post_edit(post_id):
         content = request.form['content']
         post.title = title
         post.content = content
-        image = request.files['image']
+        image = request.files.get('image')
         if image:
             for old_image in post.images:
                 db.session.delete(old_image)
@@ -67,6 +67,8 @@ def post_edit(post_id):
             db.session.add(image_file)
         db.session.add(post)
         db.session.commit()
+        if post.parent:
+            return redirect(url_for('main.post', post_id=post.parent.id))
         return redirect(url_for('main.post', post_id=post.id))
     return render_template('post_edit.html', post=post)
 
@@ -81,6 +83,8 @@ def post_delete(post_id):
     if request.method == 'POST':
         db.session.delete(post)
         db.session.commit()
+        if post.parent:
+            return redirect(url_for('main.post', post_id=post.parent.id))
         return redirect(url_for('main.posts'))
     return render_template('post_delete.html', post=post)
 
@@ -116,3 +120,34 @@ def image(image_id):
         return render_template('404.html'), 404
     # Set content type to image/jpeg
     return image.image, 200, {'Content-Type': 'image/jpeg'}
+
+
+@main.route('/posts/<post_id>/vote', methods=['POST'])
+def vote(post_id):
+    post = Post.query.get(post_id)
+    if post is None:
+        return 'Not found', 404
+    if current_user.is_anonymous:
+        return 'Forbidden', 403
+    if 'action' not in request.form:
+        return 'Bad request', 400
+    old_vote = Vote.query.filter(Vote.user == current_user, Vote.post == post).first()
+    action = request.form['action']
+    if action not in ['remove', 'upvote', 'downvote']:
+        return 'Bad request', 400
+    if old_vote:
+        db.session.delete(old_vote)
+        db.session.commit()
+    if action == 'remove':
+        vote = None
+    elif action == 'upvote':
+        vote = Vote(post=post, user=current_user, is_upvote=True)
+    else:
+        vote = Vote(post=post, user=current_user, is_upvote=False)
+    if vote:
+        db.session.add(vote)
+        db.session.commit()
+    url = url_for('main.post', post_id=post.top().id) + f'#post-{post.id}'
+    return redirect(url)
+
+
